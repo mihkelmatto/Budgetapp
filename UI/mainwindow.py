b8a43e31dd2e4203955a6482dc46b06ca2cb7de6
Mihkel <211209697+mihkelmatto@users.noreply.github.com>
2025-07-29 22:04:47 +0300

    Moved scrollarea creation to widgets.midsectionUI (duplicates a content widget from another UI function)
    Created content widget midsection.expenses
    
    TODO: Redesign style sheets

--- UI/mainwindow.py
+++ UI/mainwindow.py
@@ -4,14 +4,14 @@ from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout
 from utils.filepath import root_directory
 
 from UI.header import headerUI
-from UI.midsection import expensesUI, incomeUI, accountsUI, statisticsUI
+from UI.midsection import midsectionUI
 from UI.footer import footerUI
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.resize(1000, 600)
+        self.resize(1200, 700)
         self.setWindowTitle("Budget app")
         self.setWindowIcon(QIcon(root_directory("Resources", "Icons", "windowicon.png")))
         self.initUI()
@@ -27,7 +27,7 @@ class MainWindow(QMainWindow):
 
         # define variables, objects
         header: QWidget = headerUI(self)
-        midsection: QWidget = expensesUI(self)
+        midsection: QWidget = midsectionUI(self)
         footer: QWidget = footerUI(self)
 
         # add to layout, layout settings
@@ -57,6 +57,14 @@ class MainWindow(QMainWindow):
                 padding-left: 5px;
                 font-weight: bold;
             }
+            
+            /* footer settings */
+            #footer, #footer QLabel, #footer QPushButton{
+                background-color: hsl(0, 0%, 12%);
+            }
+            #footer QPushButton{
+                font-size: 30px;
+            }
 
             /* midsection settings */            
             #card{
@@ -66,6 +74,7 @@ class MainWindow(QMainWindow):
             #card, #card QLabel, #card QWidget{
                 background-color: hsl(0, 0%, 12%);
             }
+            
             QScrollArea{
                 border: none;
             }
@@ -82,11 +91,13 @@ class MainWindow(QMainWindow):
                 background: hsl(0, 0%, 8%);
                 border-radius: 5px;
             }
-            /* footer settings */
-            #footer, #footer QLabel, #footer QPushButton{
-                background-color: hsl(0, 0%, 12%);
+            QProgressBar{
+                border: 2px solid;
+                border-radius: 5px;
+                background-color: hsl(0, 0%, 8%);   
+                font-size: 15px;
             }
-            #footer QPushButton{
-                font-size: 30px;
+            QProgressBar::chunk{
+                background-color: grey;
             }
                 """)
--- UI/midsection.py
+++ UI/midsection.py
@@ -1,29 +1,58 @@
-from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QHBoxLayout, QScrollArea
+from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QHBoxLayout, QScrollArea, QProgressBar
+from PyQt5.QtCore import Qt
 
 from UI.widgets import widgets_cardbase
 
-
-def expensesUI(self) -> QScrollArea:
+def midsectionUI(self) -> QScrollArea:
     # create a scrollable area and its content widget
     scrollarea = QScrollArea(self)
     scrollarea.setWidgetResizable(True)
 
-    expenseswidget = QWidget()
-    layout = QVBoxLayout(expenseswidget)
+    midsection = QWidget()
+    layout = QVBoxLayout(midsection)
 
     # add items to the scrollable area
     for counter in range(6):
-        content_placeholder = QLabel("content")
+        content_placeholder = expenses()
         layout.addWidget(widgets_cardbase("taun.gif", content_placeholder))
 
     # set the content widget inside the scroll area
-    scrollarea.setWidget(expenseswidget)
+    scrollarea.setWidget(midsection)
     return scrollarea
 
-def incomeUI(self) -> QWidget:
+def expenses() -> QWidget:
+    def create_progressbar(current, max) -> QProgressBar:
+        progressbar = QProgressBar()
+        progressbar.setMaximum(max)
+        progressbar.setValue(current)
+        progressbar.setFormat(f"{current} / {max} €")
+        progressbar.setAlignment(Qt.AlignCenter)
+
+        return progressbar
+
+    expenseswidget = QWidget()
+    layout = QVBoxLayout(expenseswidget)
+
+    title = QLabel("Title")
+    subtitle = QLabel("Subtitle")
+    progressbar = create_progressbar(40, 100)
+    account = QLabel("Account")
+    maxlabel = QLabel("max")
+
+    for item in (title, subtitle, progressbar, account, maxlabel):
+        layout.addWidget(item)
+
+    expenseswidget.setObjectName("expenseswidget")
+    title.setObjectName("title")
+
+    return expenseswidget
+
+
+
+def income(self) -> QWidget:
     pass
 
-def accountsUI(self) -> QWidget:
+def accounts(self) -> QWidget:
     pass
 
 def statisticsUI(self) -> QWidget:
